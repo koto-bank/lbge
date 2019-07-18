1d05dc2c2c914b71ddf4609b725b8f2550c1465a
Lämppi Lütti <mahinator1999@gmail.com>
2019-07-18 18:31:37 +0300

    Fix and update tests

--- engine/image-loader/t/image-loader-test.lisp
+++ engine/image-loader/t/image-loader-test.lisp
@@ -5,26 +5,19 @@
 (deftest make-image-test
   (ok (instance-of-p
           'lbge.image-loader::image
-         (make-image :width 50 :height 50 :channels "rgb8" :data "raw-data"))))
+         (make-image :width 50 :height 50 :channels "rgba8" :data (vector 1 2 3 4)))))
 
 (deftest load-image-test
   (ok (instance-of-p
           'lbge.image-loader::image
           (load-image "test-file.tga")))
   (testing "Error throws"
-    (ok (signals (load-image 1)))
-    (ok (signals (load-image 'a)))
-    (ok (signals (load-image '())))
-    (ok (signals (load-image '(a b c))))
-    (ok (signals (load-image :a)))
-    (ok (signals (load-image "test-file.png"))))) ;; While png isn't support
-
-(deftest cond-extension
-    (ok (expands
-         '(lbge.image-loader::cond-extension "/path/to/file" (".ext" (some code)))
-         '(if (search ".ext" path :from-end t)
-            (some code)
-            (lbge.image-loader::cond-extension path nil)))))
+    (ok (signals (lbge.image-loader:load-image 1)))
+    (ok (signals (lbge.image-loader:load-image 'a)))
+    (ok (signals (lbge.image-loader:load-image '())))
+    (ok (signals (lbge.image-loader:load-image '(a b c))))
+    (ok (signals (lbge.image-loader:load-image :a)))
+    (ok (signals (lbge.image-loader:load-image "test-file.png"))))) ;; While png isn't support
 
 
 (defun instance-of-p (class obj)
--- engine/image-loader/t/tga-test.lisp
+++ engine/image-loader/t/tga-test.lisp
@@ -7,7 +7,7 @@
       (ok (instance-of-p 'lbge.image-loader::image image))
       (ok (and (= 128 (width  image))
                (= 128 (height image))
-               (= "rgb8" (channels image)))))
+               (= "rgb8" (channels image))))))
 
 (defun instance-of-p (class obj)
   (eq class (type-of obj)))
