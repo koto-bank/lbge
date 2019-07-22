091f8a8b12550648519d5c063fc1b9856ade099d
Vladimir Ivanov <me@succ.dk>
2019-07-22 21:07:09 +0300

    Fix vector methods and add new methods
    Fixed names for some of the vector methods so they don't clash
    with built-ins. Also fixed a typo in (cross). Added methods to
    turn a vector into a list and vice versa. Also added a swizzle
    prototype that doesn't work as of yet.

--- engine/math/vector.lisp
+++ engine/math/vector.lisp
@@ -43,11 +43,11 @@
 (defgeneric sub (vector1 vector2)
   (:documentation "Subtract vector2 from vector1"))
 
-(defgeneric mul (vector scalar)
-  (:documentation "Multiply vector by a scalar"))
+(defgeneric mul (vector value)
+  (:documentation "Multiply vector by a scalar or each respective element"))
 
-(defgeneric div (vector scalar)
-  (:documentation "Divide vector by a scalar"))
+(defgeneric div (vector value)
+  (:documentation "Divide vector by a scalar or each respective element"))
 
 (defgeneric dot (vector1 vector2)
   (:documentation "Dot product of vector1 and vector2"))
@@ -64,14 +64,24 @@
 (defgeneric negate (vector)
   (:documentation "Negate a vector"))
 
+(defgeneric absv (vector)
+  (:documentation "Make all elemets of a vector absolute values"))
+
 (defgeneric project (vector1 vector2)
   (:documentation "Projects vector1 onto vector2"))
 
 (defgeneric eqv (vector1 vector2)
   (:documentation "Test two vectors for equality"))
 
-(defgeneric neq (vector1 vector2)
+(defgeneric neqv (vector1 vector2)
   (:documentation "Test two vectors for inequality"))
+
+(defgeneric vec-to-list (vector)
+  (:documentation "Make the vector into a list"))
+
+; doesn't work yet
+(defgeneric swizzle (vector values &optional signs)
+  (:documentation "Perform swizzling on a given vector with optional sign flips"))
 
 
 (defun make-float2 (x y)
@@ -116,36 +126,68 @@
 	       (- (w vector1) (w vector2))))
 
 
-(defmethod mul ((vector float2) scalar)
-  (make-float2 (* (x vector) scalar)
-	       (* (y vector) scalar)))
+(defmethod mul ((vector float2) (value real))
+  (make-float2 (* (x vector) value)
+	       (* (y vector) value)))
 
-(defmethod mul ((vector float3) scalar)
-  (make-float3 (* (x vector) scalar)
-	       (* (y vector) scalar)
-	       (* (z vector) scalar)))
+(defmethod mul ((vector float3) (value real))
+  (make-float3 (* (x vector) value)
+	       (* (y vector) value)
+	       (* (z vector) value)))
 
-(defmethod mul ((vector float4) scalar)
-  (make-float4 (* (x vector) scalar)
-	       (* (y vector) scalar)
-	       (* (z vector) scalar)
-	       (* (w vector) scalar)))
+(defmethod mul ((vector float4) (value real))
+  (make-float4 (* (x vector) value)
+	       (* (y vector) value)
+	       (* (z vector) value)
+	       (* (w vector) value)))
 
 
-(defmethod div ((vector float2) scalar)
-  (make-float2 (/ (x vector) scalar)
-	       (/ (y vector) scalar)))
+(defmethod div ((vector float2) (value real))
+  (make-float2 (/ (x vector) value)
+	       (/ (y vector) value)))
 
-(defmethod div ((vector float3) scalar)
-  (make-float3 (/ (x vector) scalar)
-	       (/ (y vector) scalar)
-	       (/ (z vector) scalar)))
+(defmethod div ((vector float3) (value real))
+  (make-float3 (/ (x vector) value)
+	       (/ (y vector) value)
+	       (/ (z vector) value)))
 
-(defmethod div ((vector float4) scalar)
-  (make-float4 (/ (x vector) scalar)
-	       (/ (y vector) scalar)
-	       (/ (z vector) scalar)
-	       (/ (w vector) scalar)))
+(defmethod div ((vector float4) (value real))
+  (make-float4 (/ (x vector) value)
+	       (/ (y vector) value)
+	       (/ (z vector) value)
+	       (/ (w vector) value)))
+
+
+(defmethod mul ((vector float2) (value float2))
+  (make-float2 (* (x vector) (x value))
+	       (* (y vector) (y value))))
+
+(defmethod mul ((vector float3) (value float3))
+  (make-float3 (* (x vector) (x value))
+	       (* (y vector) (y value))
+	       (* (z vector) (z value))))
+
+(defmethod mul ((vector float4) (value float4))
+  (make-float4 (* (x vector) (x value))
+	       (* (y vector) (y value))
+	       (* (z vector) (z value))
+	       (* (w vector) (w value))))
+
+
+(defmethod div ((vector float2) (value float2))
+  (make-float2 (/ (x vector) (x value))
+	       (/ (y vector) (y value))))
+
+(defmethod div ((vector float3) (value float3))
+  (make-float3 (/ (x vector) (x value))
+	       (/ (y vector) (y value))
+	       (/ (z vector) (z value))))
+
+(defmethod div ((vector float4) (value float4))
+  (make-float4 (/ (x vector) (x value))
+	       (/ (y vector) (y value))
+	       (/ (z vector) (z value))
+	       (/ (w vector) (w value))))
 
 
 (defmethod dot ((vector1 float2) vector2)
@@ -167,17 +209,49 @@
 (defmethod norm ((vector float2))
   (sqrt (+ (expt (x vector) 2)
 	   (expt (y vector) 2))))
-
+  
 (defmethod norm ((vector float3))
   (sqrt (+ (expt (x vector) 2)
 	   (expt (y vector) 2)
 	   (expt (z vector) 2))))
-
+  
 (defmethod norm ((vector float4))
   (sqrt (+ (expt (x vector) 2)
 	   (expt (y vector) 2)
 	   (expt (z vector) 2)
 	   (expt (w vector) 2))))
+
+
+(defmethod negate ((vector float2))
+  (make-float2 (- (x vector))
+	       (- (y vector))))
+
+(defmethod negate ((vector float3))
+  (make-float3 (- (x vector))
+	       (- (y vector))
+	       (- (z vector))))
+
+(defmethod negate ((vector float4))
+  (make-float4 (- (x vector))
+	       (- (y vector))
+	       (- (z vector))
+	       (- (w vector))))
+
+
+(defmethod absv ((vector float2))
+  (make-float2 (abs (x vector))
+	       (abs (y vector))))
+
+(defmethod absv ((vector float3))
+  (make-float3 (abs (x vector))
+	       (abs (y vector))
+	       (abs (z vector))))
+
+(defmethod absv ((vector float4))
+  (make-float4 (abs (x vector))
+	       (abs (y vector))
+	       (abs (z vector))
+	       (abs (w vector))))
 
 
 (defmethod eqv ((vector1 float2) vector2)
@@ -196,6 +270,10 @@
        (= (w vector1) (w vector2))))
 
 
+(defmethod neqv (vector1 vector2)
+  (not (eqv vector1 vector2)))
+
+
 (defun cross (vector1 vector2)
   (make-float3 (- (* (y vector1) (z vector2))
 		  (* (z vector1) (y vector2)))
@@ -210,13 +288,44 @@
 
 
 (defmethod angle (vector1 vector2)
-  (acos (/ (dot vector1 vector 2)
+  (acos (/ (dot vector1 vector2)
 	   (* (norm vector1)
 	      (norm vector2)))))
 
 
 (defmethod project (vector1 vector2)
   (dot vector1 (normalize vector2)))
+
+
+(defmethod vec-to-list ((vector float2))
+  (list (x vector) (y vector)))
+
+(defmethod vec-to-list ((vector float3))
+  (list (x vector) (y vector) (z vector)))
+
+(defmethod vec-to-list ((vector float4))
+  (list (x vector) (y vector) (z vector) (w vector)))
+
+
+(defun list-to-vec (list)
+  (ecase (list-length list)
+    (2 (make-float2 (car list) (cdar list)))
+    (3 (make-float3 (car list) (cdar list) (cddar list)))
+    (4 (make-float4 (car list) (cdar list) (cddar list) (cdddar list)))))
+
+
+; doesnt work yet
+(defmethod swizzle ((vector float2) values &optional signs)
+    (with-slots (x y) vector
+    (map nil
+     (lambda (c s)
+       (ecase c
+	 (#\X (funcall (intern (string s)) x))
+	 (#\Y (funcall (intern (string s)) y))))
+     (symbol-name values)
+     (if (null signs)
+	 "++"
+	 (symbol-name signs)))))
 
 
 (defun float2-zero () (make-instance 'float2))
