6f7a3a8d6903148073b2dca65fb519268c90ea91
Vladimir Ivanov <me@succ.dk>
2019-08-14 13:27:39 +0300

    Fix formatting in all of the math module and tests

--- engine/math/t/vector.lisp
+++ engine/math/t/vector.lisp
@@ -5,109 +5,109 @@
 
 (deftest add-test
   (testing "2d vector addition"
-	   (ok (eqv (add (make-float2 1 2)
-			 (make-float2 3 4))
-		    (make-float2 4 6))))
+    (ok (eqv (add (make-float2 1 2)
+                  (make-float2 3 4))
+             (make-float2 4 6))))
   
   (testing "3d vector addition"
-	   (ok (eqv (add (make-float3 1 2 3)
-			 (make-float3 4 5 6))
-		    (make-float3 5 7 9))))
+    (ok (eqv (add (make-float3 1 2 3)
+                  (make-float3 4 5 6))
+             (make-float3 5 7 9))))
   
   (testing "4d vector addition"
-	   (ok (eqv (add (make-float4 1 2 3 4)
-			 (make-float4 5 6 7 8))
-		    (make-float4 6 8 10 12)))))
+    (ok (eqv (add (make-float4 1 2 3 4)
+                  (make-float4 5 6 7 8))
+             (make-float4 6 8 10 12)))))
 
 
 (deftest sub-test
   (testing "2d vector subtraction"
-	   (ok (eqv (sub (make-float2 1 2)
-			 (make-float2 3 4))
-		    (make-float2 -2 -2))))
+    (ok (eqv (sub (make-float2 1 2)
+                  (make-float2 3 4))
+             (make-float2 -2 -2))))
 
   (testing "3d vector subtraction"
-	   (ok (eqv (sub (make-float3 1 2 3)
-			 (make-float3 4 5 6))
-		    (make-float3 -3 -3 -3))))
+    (ok (eqv (sub (make-float3 1 2 3)
+                  (make-float3 4 5 6))
+             (make-float3 -3 -3 -3))))
 
   (testing "4d vector subtraction"
-	   (ok (eqv (sub (make-float4 1 2 3 4)
-			 (make-float4 5 6 7 8))
-		    (make-float4 -4 -4 -4 -4)))))
+    (ok (eqv (sub (make-float4 1 2 3 4)
+                  (make-float4 5 6 7 8))
+             (make-float4 -4 -4 -4 -4)))))
 
 
 (deftest mul-test
   (testing "2d vector multiplication"
-	   (ok (eqv (mul (make-float2 1 2) 2)
-		    (make-float2 2 3))))
+    (ok (eqv (mul (make-float2 1 2) 2)
+             (make-float2 2 3))))
   
   (testing "3d vector multiplication"
-	   (ok (eqv (mul (make-float3 1 2 3) 2)
-		   (make-float3 2 4 6))))
+    (ok (eqv (mul (make-float3 1 2 3) 2)
+             (make-float3 2 4 6))))
   
   (testing "4d vector multiplication"
-	   (ok (eqv (mul (make-float4 1 2 3 4) 2)
-		    (make-float4 2 4 6 8))))
+    (ok (eqv (mul (make-float4 1 2 3 4) 2)
+             (make-float4 2 4 6 8))))
   
   (testing "2d by 2d vector multiplication"
-	   (ok (eqv (mul (make-float2 1 2)
-			 (make-float2 3 4))
-		    (make-float2 3 8))))
+    (ok (eqv (mul (make-float2 1 2)
+                  (make-float2 3 4))
+             (make-float2 3 8))))
   
   (testing "3d by 3d vector multiplication"
-	   (ok (eqv (mul (make-float3 1 2 3)
-			 (make-float3 4 5 6))
-		    (make-float3 4 10 18))))
+    (ok (eqv (mul (make-float3 1 2 3)
+                  (make-float3 4 5 6))
+             (make-float3 4 10 18))))
   
   (testing "4d by 4d vector multiplication"
-	   (ok (eqv (mul (make-float4 1 2 3 4)
-			 (make-float4 5 6 7 8))
-		    (make-float4 5 12 21 32)))))
+    (ok (eqv (mul (make-float4 1 2 3 4)
+                  (make-float4 5 6 7 8))
+             (make-float4 5 12 21 32)))))
 
 
 (deftest div-test
   (testing "2d vector division"
-	   (ok (eqv (div (make-float2 2 4)
-			 2)
-		    (make-float2 1 2))))
+    (ok (eqv (div (make-float2 2 4)
+                  2)
+             (make-float2 1 2))))
   
   (testing "3d vector division"
-	   (ok (eqv (div (make-float3 2 4 6)
-			 2)
-		    (make-float3 1 2 3))))
+    (ok (eqv (div (make-float3 2 4 6)
+                  2)
+             (make-float3 1 2 3))))
   
   (testing "4d vector division"
-	   (ok (eqv (div (make-float4 2 4 6 8)
-			 2)
-		    (make-float4 1 2 3 4))))
+    (ok (eqv (div (make-float4 2 4 6 8)
+                  2)
+             (make-float4 1 2 3 4))))
   
   (testing "2d by 2d vector division"
-	   (ok (eqv (mul (make-float2 3 8)
-			 (make-float2 3 4))
-		    (make-float2 1 2))))
+    (ok (eqv (mul (make-float2 3 8)
+                  (make-float2 3 4))
+             (make-float2 1 2))))
   
   (testing "3d by 3d vector division"
-	   (ok (eqv (mul (make-float3 4 10 18)
-			 (make-float3 4 5 6))
-		    (make-float3 1 2 3))))
+    (ok (eqv (mul (make-float3 4 10 18)
+                  (make-float3 4 5 6))
+             (make-float3 1 2 3))))
   
   (testing "4d by 4d vector division"
-	   (ok (eqv (mul (make-float4 5 12 21 32)
-			 (make-float4 5 6 7 8))
-		    (make-float4 1 2 3 4)))))
+    (ok (eqv (mul (make-float4 5 12 21 32)
+                  (make-float4 5 6 7 8))
+             (make-float4 1 2 3 4)))))
 
 
 (deftest dot-test
   (testing "2d dot product"
-	   (ok (= (dot (make-float2 1 2)
-		       (make-float2 3 4))
-		  11)))
+    (ok (= (dot (make-float2 1 2)
+                (make-float2 3 4))
+           11)))
   
   (testing "3d dot product"
-	   (ok (= (dot (make-float3 1 2 3)
-		       (make-float3 4 5 6))
-		  32)))
+    (ok (= (dot (make-float3 1 2 3)
+                (make-float3 4 5 6))
+           32)))
   
   (testing "4d dot product"
     (ok (= (dot (make-float4 1 2 3 4)
@@ -117,23 +117,23 @@
 
 (deftest cross-test
   (testing "Cross product"
-	   (ok (eqv (cross (make-float3 1 2 3)
-			   (make-float3 4 5 6))
-		    (make-float3 -3 6 -3)))))
+    (ok (eqv (cross (make-float3 1 2 3)
+                    (make-float3 4 5 6))
+             (make-float3 -3 6 -3)))))
 
 
 (deftest norm-test
   (testing "2d vector norm"
-	   (ok (= (norm (make-float2 1 2))
-		  (sqrt 5))))
+    (ok (= (norm (make-float2 1 2))
+           (sqrt 5))))
   
   (testing "3d vector norm"
-	   (ok (= (norm (make-float3 1 2 3))
-		  (sqrt 14))))
+    (ok (= (norm (make-float3 1 2 3))
+           (sqrt 14))))
   
   (testing "4d vector norm"
-	   (ok (= (norm (make-float4 1 2 3 4))
-		  (sqrt 30)))))
+    (ok (= (norm (make-float4 1 2 3 4))
+           (sqrt 30)))))
 
 
 (deftest angle-test
@@ -155,43 +155,43 @@
 
 (deftest negv-test
   (testing "2d vector negation"
-	   (ok (eqv (negv (make-float2 1 2))
-		    (make-float2 -1 -2))))
+    (ok (eqv (negv (make-float2 1 2))
+             (make-float2 -1 -2))))
   
   (testing "3d vector negation"
-	   (ok (eqv (negv (make-float3 1 2 3))
-		    (make-float3 -1 -2 -3))))
+    (ok (eqv (negv (make-float3 1 2 3))
+             (make-float3 -1 -2 -3))))
   
   (testing "4d vector negation"
-	   (ok (eqv (negv (make-float4 1 2 3 4))
-		    (make-float4 -1 -2 -3 -4)))))
+    (ok (eqv (negv (make-float4 1 2 3 4))
+             (make-float4 -1 -2 -3 -4)))))
 
 (deftest absv-test
   (testing "2d vector abs"
-	   (ok (eqv (absv (make-float2 -1 -2))
-		    (make-float2 1 2))))
+    (ok (eqv (absv (make-float2 -1 -2))
+             (make-float2 1 2))))
   
   (testing "3d vector abs"
-	   (ok (eqv (absv (make-float3 -1 -2 -3))
-		    (make-float3 1 2 3))))
+    (ok (eqv (absv (make-float3 -1 -2 -3))
+             (make-float3 1 2 3))))
   
   (testing "4d vector abs"
-	   (ok (eqv (absv (make-float4 -1 -2 -3 -4))
-		    (make-float4 1 2 3 4)))))
+    (ok (eqv (absv (make-float4 -1 -2 -3 -4))
+             (make-float4 1 2 3 4)))))
 
 
 (deftest project-test
   (testing "2d vector projection"
-	   (ok (eqv (project (make-float2 1 2)
-			     (make-float2 3 4))
-		   (make-float2 (/ 33 25) (/ 44 25)))))
+    (ok (eqv (project (make-float2 1 2)
+                      (make-float2 3 4))
+             (make-float2 (/ 33 25) (/ 44 25)))))
   
   (testing "3d vector projection"
-	   (ok (eqv (project (make-float3 1 2 3)
-			     (make-float3 4 5 6))
-		    (make-float3 (/ 128 77) (/ 160 77) (/ 192 77)))))
+    (ok (eqv (project (make-float3 1 2 3)
+                      (make-float3 4 5 6))
+             (make-float3 (/ 128 77) (/ 160 77) (/ 192 77)))))
 
   (testing "4d vector projection"
-	   (ok (eqv (project (make-float4 1 2 3 4)
-			     (make-float4 5 6 7 8))
-		   (make-float4 (/ 175 87) (/ 70 29) (/ 245 87) (/ 280 87))))))
+    (ok (eqv (project (make-float4 1 2 3 4)
+                      (make-float4 5 6 7 8))
+             (make-float4 (/ 175 87) (/ 70 29) (/ 245 87) (/ 280 87))))))
