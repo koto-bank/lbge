63775c1c7aa52ba1d87931dac6304c456b9097e1
Lämppi Lütti <mahinator1999@gmail.com>
2019-08-17 18:13:07 +0300

    Fix ccase in image-loader

--- engine/image-loader/image-loader.lisp
+++ engine/image-loader/image-loader.lisp
@@ -26,5 +26,5 @@
 (defun load-image (path)
   "Takes path to an image and returns `image` structure.
    If the image format is unsupported throws error."
-  (ccase (pathname-type path)
-    ("tga" (lbge.image-loader.tga:tga path))))
+  (ccase (intern (pathname-type path))
+    ('|tga| (lbge.image-loader.tga:tga path))))
