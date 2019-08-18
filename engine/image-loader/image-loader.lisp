5ad53f5da24f90a8baa5cd9421a8ce2db0bc8681
Lämppi Lütti <mahinator1999@gmail.com>
2019-08-18 15:50:17 +0300

    Replace 'equal to 'string= in load-image

--- engine/image-loader/image-loader.lisp
+++ engine/image-loader/image-loader.lisp
@@ -26,5 +26,5 @@
 (defun load-image (path)
   "Takes path to an image and returns `image` structure.
    If the image format is unsupported throws error."
-  (switch ((pathname-type path) :test 'equal)
+  (switch ((pathname-type path) :test 'string=)
     ("tga" (lbge.image-loader.tga:tga path))))
