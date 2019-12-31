b586706458117e73651456a818f2646d77e7886e
alex-eg <alexey.e.egorov@gmail.com>
2019-12-31 20:30:24 +0300

    Add more filesystem functions

--- engine/filesystem/misc.lisp
+++ engine/filesystem/misc.lisp
@@ -6,3 +6,37 @@
     (if (null app-root-arg)
       (asdf:system-source-directory 'lbge)
       (make-pathname :directory app-root-arg))))
+
+(defun is-exist (path)
+  (or (is-file path)
+      (is-dir path)))
+
+(defun is-file (path)
+  "Check if file/dir exists"
+  (uiop:file-exists-p path))
+
+(defun is-dir (path)
+  "Check if path exists and is directory"
+  (uiop:directory-exists-p path))
+
+;;; Asset path defined by root and relative path,
+;;; e.g. ":textures/sprites/whatever"
+(defun parse-asset-path (asset-path)
+  "Parse asset path and return pair (:root-name . #P\"rel-path\")"
+  (cl-ppcre:register-groups-bind (root path)
+      ("^(.*?)/(.*)$" asset-path)
+    (cons (read-from-string root) (uiop:make-pathname* :name path))))
+
+(defun merge-paths (path-1 path-2)
+  (uiop:merge-pathnames* path-2 (uiop:ensure-directory-pathname path-1)))
+
+(defun path-eq (path-1 path-2)
+  (uiop:pathname-equal
+   (uiop:truenamize path-1)
+   (uiop:truenamize path-2)))
+
+(defun make-path (&key name dir)
+  (uiop:make-pathname* :name name :directory dir))
+
+(defun root-relative-path (path)
+  (merge-paths (get-app-root) path))
--- engine/filesystem/package.lisp
+++ engine/filesystem/package.lisp
@@ -1,3 +1,12 @@
 (defpackage :lbge.filesystem
   (:use :cl)
-  (:export :get-app-root))
+  (:export
+   :get-app-root
+   :is-exist
+   :is-file
+   :is-dir
+   :parse-asset-path
+   :merge-paths
+   :path-eq
+   :make-path
+   :root-relative-path))
