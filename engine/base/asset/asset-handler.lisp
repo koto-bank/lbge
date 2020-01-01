b414cfe43914f64cc0462c43c4594ec65b115aa4
alex-eg <alexey.e.egorov@gmail.com>
2020-01-01 14:48:17 +0300

    Asset manager now actually works (and quite reliably!)

--- engine/base/asset/asset-handler.lisp
+++ engine/base/asset/asset-handler.lisp
@@ -4,5 +4,16 @@
   ()
   (:documentation "Base class for all asset handlers"))
 
-(defgeneric handler-load-asset (handler key))
-(defgeneric handler-create-asset (handler key))
+(defgeneric handler-get-asset (handler asset-manager key)
+  (:documentation "Create or load assets by key.
+Implementation decides which asset keys are supported, what options
+are needed for loading, and so on"))
+
+(defun find-asset-file-by-path (asset-roots path)
+  "Find asset file by asset path"
+  (let* ((parsed-path (parse-asset-path path))
+         (root-key (car parsed-path))
+         (path (cdr parsed-path))
+         (root (assoc root-key asset-roots)))
+    (assert root nil "Root with key ~S is not registered" root-key)
+    (is-file (merge-paths (cdr root) path))))
