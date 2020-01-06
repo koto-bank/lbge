847b208b297140a3bffc712182d0cb5a5bc73a3d
alex-eg <alexey.e.egorov@gmail.com>
2020-01-06 21:33:01 +0300

    Add hook usage to test application

--- a/engine-test/main.lisp
+++ a/engine-test/main.lisp
@@ -4,9 +4,14 @@
 
 (in-package :lbge-engine-test)
 
+(defun print-win-info ()
+  (multiple-value-bind (major minor patch) (sdl2:version)
+    (format t "Running SDL version: ~S.~S.~S~%" major minor patch)))
+
 (defun run ()
   (lbge.engine:delete-engine)
   (lbge.engine:make-engine)
+  (lbge.engine:link :before-start #'print-win-info)
   (lbge.engine.events:add-event-handlers
     (:keyup
      (:keysym keysym)
