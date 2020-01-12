c93999eefd27168ab927b9ff78a3ce30ee044d63
Lämppi Lütti <mahinator1999@gmail.com>
2020-01-12 18:02:38 +0300

    59/Update hash test

--- engine/base/t/hash-map-test.lisp
+++ engine/base/t/hash-map-test.lisp
@@ -3,15 +3,10 @@
 
 (in-package :lbge.test.hash)
 
-(deftest hash-make-equal-test
-    (ok (eq (equalp (make-hash-table) (make-hash-table))
-            (equal-hash (make-hash) (make-hash)))))
-
-(deftest hash-set-test
-  (let ((h1 (set-hash (make-hash) '((:a 1) (:b 2)))))
-    (ok (equal-hash h1
-                    (set-hash (make-hash) '((:a 1) (:b 2)))))))
-
-(deftest get-hash-test
-  (let ((h1 (set-hash (make-hash) '((:a 1)))))
-    (ok (eq 1 (get-hash h1 :a)))))
+(deftest hash-test
+    (let ((h (hash-make '((:a 1) (:b 2)))))
+      (ok (eq (hash-get h :a) 1))
+      (ok (null (hash-get h :c)))
+      (ok (eq (hash-get h :c 3) 3))
+      (ok (hash-equal (hash-set (hash-make) '((:a 1) (:b 2))) h))
+      (ng (hash-equal (hash-set (hash-make) '((:c 3))) h))))
