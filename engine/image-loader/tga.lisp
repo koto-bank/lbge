d78221f66f43a536ccbbc09490cae38de4d8367c
Lämppi Lütti <mahinator1999@gmail.com>
2020-01-23 23:22:05 +0300

    A

--- engine/image-loader/tga.lisp
+++ engine/image-loader/tga.lisp
@@ -14,17 +14,31 @@
                          (aref raw 16))) ; Bytes per pixel
          (screen-destination (logand (aref raw 17) #0b00110000))
          (data-start-pos (get-data-start-pos raw))
-         (data (if * (> 3 (aref raw 2)) ; RLE check
-                   (decode-rle raw data-start-pos bytes-count)
+         (data (if (> 3 (aref raw 2)) ; RLE check
+                   (decode-rle (subseq raw raw data-start-pos) bytes-count)
                    (subseq raw data-start-pos (+ data-start-pos bytes-count)))))
     (cond ;; TODO
-      ((= #0b00 screen-destination)q)
+      ((= #0b00 screen-destination))
       ((= #0b01 screen-destination))
       ((= #0b10 screen-destination) data)
       ((= #0b11 screen-destination)))))
 
-(defun decode-rle (raw start-pos bytes-count))
-  ;; TODO
+(defun decode-rle (raw bytes-count &optional decoded)
+  (let* ((first-byte (aref raw 1))
+         (rle? (logandg first-byte #0b10000000))
+         (count (logand first-byte #0b11111110)))
+    (if (= decoded bytes-count)
+        decoded
+        (decode-rle (if rle?
+                     (subseq raw 3)
+                     (subseq raw (1+ count)))
+                    bytes-count
+                    (if rle?
+                        (reduce #'cons decoded (make-list count :initial-element (aref raw 2)))
+                        (reduce #'cons
+                                decoded
+                                :initial-value (subseq raw 1 count)
+                                :from-end t))))))
 
 (defun get-data-start-pos (raw)
   (+ 17 ; Header last byte position
@@ -40,5 +54,3 @@
 (defun make-16bits (f-byte s-byte)
   "Merge two bytes to one 2 byte value."
   (+ (ash f-byte 8) s-byte))
-
-
