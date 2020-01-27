1f02e80c8f9da35301f2722cc192e31ccbd5ac00
alex-eg <alexey.e.egorov@gmail.com>
2020-01-27 21:52:57 +0300

    Replace render-test long package names with nicknames

--- a/render-test/main.lisp
+++ a/render-test/main.lisp
@@ -1,7 +1,11 @@
 (defpackage :lbge-render-test
   (:use :cl)
-  (:local-nicknames (:b :lbge.render.backend)
-                    (:m :lbge.math)
+  (:local-nicknames (:m :lbge.math)
+                    (:le :lbge.engine)
+                    (:a :lbge.render)
+                    (:r :lbge.render)
+                    (:b :lbge.render.backend)
+                    (:s :lbge.render.shader)
                     (:u :lbge.utils))
   (:export :run))
 
@@ -9,64 +13,69 @@
 
 (defun run ()
   (log:config :info)
-  (lbge.engine:delete-engine)
-  (lbge.engine:make-engine)
-  (lbge.engine.events:add-event-handlers
+  (le:delete-engine)
+  (le:make-engine)
+  (le.events:add-event-handlers
     (:keyup
      (:keysym keysym)
      (when (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-escape)
        (sdl2:push-event :quit))
      (format t "Pressed ~S key~%" (sdl2:scancode keysym))))
-  (let ((r (lbge.render:make-renderer :gl))
-        (a (lbge.asset:make-asset-manager))
-        (c (lbge.render:make-ortho-camera :left 0.0f0 :right 1.0f0
-                                          :top 1.0f0 :bottom 0.0f0
-                                          :near 0.1f0 :far 1.0f0)))
-    (lbge.filesystem:set-app-root-to-system 'lbge-render-test)
+  (let ((r (r:make-renderer :gl))
+        (a (a:make-asset-manager))
+        (c (r:make-ortho-camera :left 0.0f0 :right 1.0f0
+                                :top 1.0f0 :bottom 0.0f0
+                                :near 0.1f0 :far 1.0f0)))
+    (f:set-app-root-to-system 'lbge-render-test)
     ;; Setup asset manager
-    (lbge.asset:add-root a :root ".")
-    (lbge.asset:add-handler a (make-instance 'lbge.asset:glsl-asset-handler))
-    (lbge.engine:add-manager a)
+    (a:add-root a :root ".")
+    (a:add-handler a (make-instance 'lbge.asset:glsl-asset-handler))
+    (le:add-manager a)
 
     ;; Install
     (let ((frag-shader-asset
-            (lbge.asset:get-asset a (lbge.asset:make-asset-key :glsl-source :disk ":root/frag.glsl")))
+            (a:get-asset a (a:make-asset-key :glsl-source :disk ":root/frag.glsl")))
           (vert-shader-asset
-            (lbge.asset:get-asset a (lbge.asset:make-asset-key :glsl-source :disk ":root/vert.glsl"))))
+            (a:get-asset a (lbge.asset:make-asset-key :glsl-source :disk ":root/vert.glsl")))
+          (shader (b:make-shader)))
       (log:info "Fragment shader:")
       (let ((lines (u:merge-lines (lbge.asset:asset-data frag-shader-asset))))
         (log:info lines))
       (log:info "Vertex shader:")
       (let ((lines (u:merge-lines (lbge.asset:asset-data vert-shader-asset))))
-        (log:info lines)))
-    (log:debug "Load truename: ~S" *load-truename*)
-    (lbge.engine:install-renderer r)
-    (lbge.render:add-camera r c)
-    (lbge.render:set-current-camera r c)
-    (lbge.render:add-objects
-     r
-     (list (lbge.render:make-rect :w 0.1f0 :h 0.1f0
-                                  :transform
-                                  (m:make-transform :pos (m:make-float3 0.3f0 0.3f0 0.0f0)))
-           (lbge.render:make-triangle :size 0.1f0
-                                      :transform
-                                      (m:make-transform :pos (m:make-float3 0.3f0 0.6f0 0.0f0)))
-           (lbge.render:make-ellipse :w 0.1f0 :h 0.1f0
-                                     :transform
-                                     (m:make-transform :pos (m:make-float3 0.6f0 0.6f0 0.0f0)))
-           (lbge.render:make-ring :w 0.1f0 :h 0.1f0 :thickness 0.02f0
-                                  :transform
-                                  (m:make-transform :pos (m:make-float3 0.6f0 0.3f0 0.0f0)))))
-    (lbge.engine:link :before-start
-                      (lambda ()
-                        (b:init (lbge.render:get-backend r)
-                                (lbge.engine:get-main-window))
+        (log:info lines))
+      (s:add-stage shader :vertex vert-shader-asset)
+      (s:add-stage shader :fragment frag-shader-asset)
+      (s:compile shader)
+      (s:link shader)))
+  (log:debug "Load truename: ~S" *load-truename*)
+  (le:install-renderer r)
+  (r:add-camera r c)
+  (r:set-current-camera r c)
+  (r:add-objects
+   r
+   (list (r:make-rect :w 0.1f0 :h 0.1f0
+                      :transform
+                      (m:make-transform :pos (m:make-float3 0.3f0 0.3f0 0.0f0)))
+         (r:make-triangle :size 0.1f0
+                          :transform
+                          (m:make-transform :pos (m:make-float3 0.3f0 0.6f0 0.0f0)))
+         (r:make-ellipse :w 0.1f0 :h 0.1f0
+                         :transform
+                         (m:make-transform :pos (m:make-float3 0.6f0 0.6f0 0.0f0)))
+         (r:make-ring :w 0.1f0 :h 0.1f0 :thickness 0.02f0
+                      :transform
+                      (m:make-transform :pos (m:make-float3 0.6f0 0.3f0 0.0f0)))))
+  (le:link :before-start
+           (lambda ()
+             (b:init (r:get-backend r)
+                     (le:get-main-window))
 
-                        (format t "OpenGL version string: ~a~%" (gl:gl-version))
-                        (format t "GLSL version string: ~a~%" (gl:glsl-version))
-                        (gl:clear-color 0.02f0 0.05f0 0.05f0 1.0f0)))
-    (lbge.engine:link :on-loop
-                      (lambda ()
-                        (gl:clear :color-buffer-bit)
-                        (lbge.render.backend:present (lbge.render:get-backend r)))))
-  (lbge.engine:start))
+             (format t "OpenGL version string: ~a~%" (gl:gl-version))
+             (format t "GLSL version string: ~a~%" (gl:glsl-version))
+             (gl:clear-color 0.02f0 0.05f0 0.05f0 1.0f0)))
+  (le:link :on-loop
+           (lambda ()
+             (gl:clear :color-buffer-bit)
+             (b:present (r:get-backend r)))))
+  (le:start))
