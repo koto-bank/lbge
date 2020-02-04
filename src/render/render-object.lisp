a16416a3414a91baa557062a2864235de221465a
alex-eg <alexey.e.egorov@gmail.com>
2020-02-04 20:34:49 +0300

    Add backend-data for render-objects

--- src/render/render-object.lisp
+++ src/render/render-object.lisp
@@ -2,5 +2,6 @@
 
 (defclass render-object ()
   ((vertices :documentation "Vertex array. A simple-vector")
-   (indices :documentation "Index array. A simple-vector"))
+   (indices :documentation "Index array. A simple-vector")
+   (backend-data :documentation "Backend-dependent data. Opaque"))
   (:documentation "Render object. The thing that contains backend-independent info for rendering"))
