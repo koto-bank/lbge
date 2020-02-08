91f85b67c8df0aeb0b6d6a6d249a7b69be0f01aa
alex-eg <alexey.e.egorov@gmail.com>
2020-02-08 19:46:40 +0300

    Add bind macro

--- src/base/utils.lisp
+++ src/base/utils.lisp
@@ -18,3 +18,20 @@
                          b))
           (cdr line-list)
           :initial-value (car line-list)))
+
+(defmacro bind (fun &rest args)
+  "Bind fun to provided args positionally.
+Args named _ remain unbound and should be provided in
+subsequent funcall, e.g:
+   > (funcall (bind + 1 _ 2 _) 4 5)
+   12"
+  (let ((new-arglist (list))
+        (lambda-arglist (list)))
+    (dolist (a args)
+      (if (string= (format nil "~A" a) "_")
+          (let ((var (gensym)))
+            (push var new-arglist)
+            (push var lambda-arglist))
+          (push a new-arglist)))
+    `(lambda (,@(nreverse lambda-arglist))
+       (,fun ,@(nreverse new-arglist)))))
