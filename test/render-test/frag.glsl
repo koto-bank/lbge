084dd04189a67fc5fdc58690eb235e9d53df8f64
alex-eg <alexey.e.egorov@gmail.com>
2020-02-18 23:11:25 +0300

    Simplify render-test shaders

--- test/render-test/frag.glsl
+++ test/render-test/frag.glsl
@@ -1,10 +1,8 @@
 #version 330
 
-in vec4 vert;
-
-out vec4 out_color;
+out vec4 color;
 
 void main ()
 {
-    out_color = vec4(1.0, 0.0, 0.0, 1.0);
+    color = vec4(1.0, 0.0, 0.0, 1.0);
 }
--- test/render-test/vert.glsl
+++ test/render-test/vert.glsl
@@ -2,12 +2,8 @@
 
 layout(location = 0) in vec4 vert_pos;
 
-uniform mat4 MV;
-uniform mat4 P;
-
-out vec4 vert;
+uniform mat4 model_view;
 
 void main() {
-    gl_Position = P * MV * vert_pos;
-    vert = vert_pos;
+    gl_Position = model_view * vert_pos;
 }
