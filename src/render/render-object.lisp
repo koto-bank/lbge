e95e6f5967523cec4244a2d1c0712b2e1cd4851b
alex-eg <alexey.e.egorov@gmail.com>
2020-02-19 22:42:00 +0300

    Pass provided trasnform to render-objects in primitive creation

--- src/render/render-object.lisp
+++ src/render/render-object.lisp
@@ -11,15 +11,18 @@
             :initform (make-arrray 0 :adjustable t
                                      :fill-pointer 0)
             :initarg :batches)
+   (transform :documentation "Transform matrix"
+              :initarg :transform)
    (backend-data :documentation "Backend-dependent data. Opaque"))
   (:documentation "Render object. The thing that contains backend-independent info for rendering"))
 
-(defun make-render-object (batches)
+(defun make-render-object (batches &optional (transform (m:make-transform)))
   (make-instance 'render-object
                  :batches (make-array (length batches)
                                       :initial-contents batches
                                       :adjustable t
-                                      :fill-pointer (length batches))))
+                                      :fill-pointer (length batches))
+                 :transform transform))
 
 (defun add-batch (object batch)
   (vector-push-extend batch (slot-value 'batches object)))
