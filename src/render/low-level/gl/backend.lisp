2efdcd7fed77c795a2e1a87b81d8efd343e72299
alex-eg <alexey.e.egorov@gmail.com>
2020-02-19 22:41:11 +0300

    Change renderer shader API

--- src/render/low-level/gl/backend.lisp
+++ src/render/low-level/gl/backend.lisp
@@ -26,11 +26,24 @@ info is an alist, which may contain following keys:
 
 (defmethod b:render ((backend gl-backend) renderer))
 
-(defmethod b:make-shader ((backend gl-backend))
+(defun add-shader (backend name shader)
+  (with-slots (shader-map) backend
+    (assert (null (h:hash-get shader-map name))
+            nil "Shader with name ~A already exists" name)
+    (h:hash-set shader-map name shader)))
+
+(defmethod b:make-shader ((backend gl-backend) shader-name)
   (let ((shader (make-instance 'gl-shader)))
     (setf (slot-value shader 'handle)
           (gl:create-program))
+    (add-shader backend shader-name shader)
     shader))
+
+(defmethod b:use-shader ((backend gl-backend) (shader gl-shader))
+  (with-slots (status handle) shader
+    (assert (eq :compiled status) nil
+            "Shader must be compiled in order to be used")
+    (setf (slot-value backend 'active-shader) shader)))
 
 (defmethod b:present ((backend gl-backend))
   (sb-int:with-float-traps-masked (:invalid)
