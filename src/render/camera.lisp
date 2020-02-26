81eb9e6ad20544035ecce11d7c376f027aa808ea
alex-eg <alexey.e.egorov@gmail.com>
2020-02-26 20:44:59 +0300

    Add method for camera to asjust to new aspect ratio

--- src/render/camera.lisp
+++ src/render/camera.lisp
@@ -16,6 +16,12 @@
    (near)
    (far)))
 
+(defgeneric adjust-camera-new-aspect (camera aspect preserve-fov)
+  (:documentation
+   "Adjusts camera for new aspect ratio.
+Preserve-fov is a keyword which tells which fov we should preserve, :x
+or :y"))
+
 (defun make-ortho-camera (&key left right top bottom near far view)
   (let ((cam (make-instance 'ortho-camera))
         (proj-matrix (m:make-ortho-projection
@@ -39,3 +45,33 @@
             c-near near
             c-far far))
     cam))
+
+(defmethod adjust-camera-new-aspect ((camera ortho-camera) aspect preserve-fov)
+  (with-slots (left right top bottom near far matrix) camera
+    (let*
+        ((width (- right left))
+         (height (- top bottom))
+
+         (new-width/2 (if (eq preserve-fov :x)
+                          (/ width 2)
+                          (* (/ height 2) aspect)))
+         (new-height/2 (if (eq preserve-fov :y)
+                           (/ height 2)
+                           (* (/ width 2) (/ 1 aspect))))
+
+         (new-left (- new-width/2))
+         (new-right new-width/2)
+         (new-top new-height/2)
+         (new-bottom (- new-height/2))
+         (new-matrix (m:make-ortho-projection
+                      :left new-left
+                      :right new-right
+                      :top new-top
+                      :bottom new-bottom
+                      :near near
+                      :far far)))
+      (setf matrix new-matrix
+            left new-left
+            right new-right
+            top new-top
+            bottom new-bottom))))
