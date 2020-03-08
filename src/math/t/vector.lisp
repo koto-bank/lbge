2bd8b103f3481b4c2b604da1d2f07317d8ba0ae8
alex-eg <alexey.e.egorov@gmail.com>
2020-03-08 13:02:20 +0300

    Rewrite tests to use new macro

--- src/math/t/vector.lisp
+++ src/math/t/vector.lisp
@@ -1,26 +1,23 @@
-(defpackage :lbge.test.vector
-  (:use :cl :lbge.math :rove))
-
-(in-package :lbge.test.vector)
-
-(deftest add-test
+(define-test add-test
+    (:lbge.math)
   (testing "2d vector addition"
     (ok (eqv (add (make-float2 1 2)
                   (make-float2 3 4))
              (make-float2 4 6))))
-  
+
   (testing "3d vector addition"
     (ok (eqv (add (make-float3 1 2 3)
                   (make-float3 4 5 6))
              (make-float3 5 7 9))))
-  
+
   (testing "4d vector addition"
     (ok (eqv (add (make-float4 1 2 3 4)
                   (make-float4 5 6 7 8))
              (make-float4 6 8 10 12)))))
 
 
-(deftest sub-test
+(define-test sub-test
+    (:lbge.math)
   (testing "2d vector subtraction"
     (ok (eqv (sub (make-float2 1 2)
                   (make-float2 3 4))
@@ -37,106 +34,112 @@
              (make-float4 -4 -4 -4 -4)))))
 
 
-(deftest mul-test
+(define-test mul-test
+    (:lbge.math)
   (testing "2d vector multiplication"
     (ok (eqv (mul (make-float2 1 2) 2)
              (make-float2 2 4))))
-  
+
   (testing "3d vector multiplication"
     (ok (eqv (mul (make-float3 1 2 3) 2)
              (make-float3 2 4 6))))
-  
+
   (testing "4d vector multiplication"
     (ok (eqv (mul (make-float4 1 2 3 4) 2)
              (make-float4 2 4 6 8))))
-  
+
   (testing "2d by 2d vector multiplication"
     (ok (eqv (mul (make-float2 1 2)
                   (make-float2 3 4))
              (make-float2 3 8))))
-  
+
   (testing "3d by 3d vector multiplication"
     (ok (eqv (mul (make-float3 1 2 3)
                   (make-float3 4 5 6))
              (make-float3 4 10 18))))
-  
+
   (testing "4d by 4d vector multiplication"
     (ok (eqv (mul (make-float4 1 2 3 4)
                   (make-float4 5 6 7 8))
              (make-float4 5 12 21 32)))))
 
 
-(deftest div-test
+(define-test div-test
+    (:lbge.math)
   (testing "2d vector division"
     (ok (eqv (div (make-float2 2 4)
                   2)
              (make-float2 1 2))))
-  
+
   (testing "3d vector division"
     (ok (eqv (div (make-float3 2 4 6)
                   2)
              (make-float3 1 2 3))))
-  
+
   (testing "4d vector division"
     (ok (eqv (div (make-float4 2 4 6 8)
                   2)
              (make-float4 1 2 3 4))))
-  
+
   (testing "2d by 2d vector division"
     (ok (eqv (div (make-float2 3 8)
                   (make-float2 3 4))
              (make-float2 1 2))))
-  
+
   (testing "3d by 3d vector division"
     (ok (eqv (div (make-float3 4 10 18)
                   (make-float3 4 5 6))
              (make-float3 1 2 3))))
-  
+
   (testing "4d by 4d vector division"
     (ok (eqv (div (make-float4 5 12 21 32)
                   (make-float4 5 6 7 8))
              (make-float4 1 2 3 4)))))
 
 
-(deftest dot-test
+(define-test dot-test
+    (:lbge.math)
   (testing "2d dot product"
     (ok (= (dot (make-float2 1 2)
                 (make-float2 3 4))
            11)))
-  
+
   (testing "3d dot product"
     (ok (= (dot (make-float3 1 2 3)
                 (make-float3 4 5 6))
            32)))
-  
+
   (testing "4d dot product"
     (ok (= (dot (make-float4 1 2 3 4)
                 (make-float4 5 6 7 8))
            70))))
 
 
-(deftest cross-test
+(define-test cross-test
+    (:lbge.math)
   (testing "Cross product"
     (ok (eqv (cross (make-float3 1 2 3)
                     (make-float3 4 5 6))
              (make-float3 -3 6 -3)))))
 
 
-(deftest norm-test
+(define-test norm-test
+    (:lbge.math)
   (testing "2d vector norm"
     (ok (eqfp (norm (make-float2 1 2))
            (sqrt 5))))
-  
+
   (testing "3d vector norm"
     (ok (eqfp (norm (make-float3 1 2 3))
            (sqrt 14))))
-  
+
   (testing "4d vector norm"
     (ok (eqfp (norm (make-float4 1 2 3 4))
            (sqrt 30)))))
 
 
-(deftest angle-test
+(define-test angle-test
+    (:lbge.math)
   (testing "Angle between 2 2d vectors"
     (ok (eqfp (angle (make-float2 1 2)
                   (make-float2 2 1))
@@ -153,40 +156,43 @@
            (acos 2/3)))))
 
 
-(deftest negv-test
+(define-test negv-test
+    (:lbge.math)
   (testing "2d vector negation"
     (ok (eqv (negv (make-float2 1 2))
              (make-float2 -1 -2))))
-  
+
   (testing "3d vector negation"
     (ok (eqv (negv (make-float3 1 2 3))
              (make-float3 -1 -2 -3))))
-  
+
   (testing "4d vector negation"
     (ok (eqv (negv (make-float4 1 2 3 4))
              (make-float4 -1 -2 -3 -4)))))
 
 
-(deftest absv-test
+(define-test absv-test
+    (:lbge.math)
   (testing "2d vector abs"
     (ok (eqv (absv (make-float2 -1 -2))
              (make-float2 1 2))))
-  
+
   (testing "3d vector abs"
     (ok (eqv (absv (make-float3 -1 -2 -3))
              (make-float3 1 2 3))))
-  
+
   (testing "4d vector abs"
     (ok (eqv (absv (make-float4 -1 -2 -3 -4))
              (make-float4 1 2 3 4)))))
 
 
-(deftest project-test
+(define-test project-test
+    (:lbge.math)
   (testing "2d vector projection"
     (ok (eqv (project (make-float2 1 2)
                       (make-float2 3 4))
              (make-float2 (float 33/25) (float 44/25)))))
-  
+
   (testing "3d vector projection"
     (ok (eqv (project (make-float3 1 2 3)
                       (make-float3 4 5 6))
