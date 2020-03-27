250b49346a04b9ecc165e3684f495702c21a39ab
alex-eg <alexey.e.egorov@gmail.com>
2020-03-27 17:54:04 +0300

    Add new ecs exports to packages.lisp

--- src/ecs/package.lisp
+++ src/ecs/package.lisp
@@ -1,2 +1,27 @@
+(defpackage :lbge.ecs.entity-storage
+  (:use :cl)
+  (:local-nicknames
+   (:ss :lbge.sparse-set))
+  (:export
+   :entity-storage
+   :delete-entity
+   :create-entity
+   :entity-existsp))
+
 (defpackage :lbge.ecs
-  (:use :cl))
+  (:use
+   :cl)
+  (:local-nicknames
+   (:ax :alexandria))
+  (:export
+   :world
+   :entity
+   :component
+   :system
+
+   :defsystem
+   :defcomponent
+
+   :create-entity
+   :delte-entity
+   :existsp))
