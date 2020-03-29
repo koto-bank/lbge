4d6a0edf6892f927d9832a5e953b66fa9d0a2d9c
alex-eg <alexey.e.egorov@gmail.com>
2020-03-29 10:00:49 +0300

    Implement ECS base system class

--- src/ecs/system.lisp
+++ src/ecs/system.lisp
@@ -1,3 +1,18 @@
 (in-package :lbge.ecs)
 
-(defclass system () ())
+(defclass system ()
+  ((component-storages
+    :documentation "List of component storages")
+   (world :documentation "World the system belongs to"
+          :reader get-world)
+   (owned-component-types
+    :documentation "List of component types, which the system owns (and thus stores)"))
+  (:documentation "Base class for all systems"))
+
+(defgeneric update (system dt)
+  (:documentation
+   "Updates the system.
+Every system must implement this method"))
+
+(defmethod update ((sys system) dt)
+  (error "All systems must implement the update method! System ~A is not an exception" sys))
