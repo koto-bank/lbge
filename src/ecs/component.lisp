b0ea19439d8be3af0faeee73e26904f8ddf83933
alex-eg <alexey.e.egorov@gmail.com>
2020-03-31 20:44:59 +0300

    Make parent entity a property of component

--- src/ecs/component.lisp
+++ src/ecs/component.lisp
@@ -1,6 +1,9 @@
 (in-package :lbge.ecs)
 
 (defclass component ()
-  (entity))
+  ((entity
+    :accessor comp-entity
+    :documentation "Entity that the component belongs to"))
+  (:documentation "Base class for all components"))
 
 (defun get-sibling (component sibling-type))
