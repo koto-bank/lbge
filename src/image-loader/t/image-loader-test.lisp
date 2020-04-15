d6032696ca4c8f1257559b564a9e69ff24e2c545
alex-eg <alexey.e.egorov@gmail.com>
2020-04-15 20:17:20 +0300

    Enable image-loader unit test back

--- src/image-loader/t/image-loader-test.lisp
+++ src/image-loader/t/image-loader-test.lisp
@@ -1,23 +1,11 @@
-(defpackage lbge.test.image-loader
-  (:use :cl :rove :lbge.image-loader :lbge.image-loader.image))
-
-(in-package :lbge.test.image-loader)
-
-(defvar *lbge-skip-test*)
-
-(defun instance-of-p (class obj)
-  (eq class (type-of obj)))
-
-(deftest load-image-test
-  (ok (instance-of-p
-       'lbge.image-loader.image::image
-       (load-image "test-file.tga")))
-  ;; While png isn't support
-  (ok (eq NIL
-          (load-image "test-file.png")))
-  (testing "Error throws"
-    (ok (signals (load-image 1)))
-    (ok (signals (load-image 'a)))
-    (ok (signals (load-image '())))
-    (ok (signals (load-image '(a b c))))
-    (ok (signals (load-image :a)))))
+(define-test image-loader-test
+    ((:i :lbge.image))
+  (testing "Loading TGA"
+    (let ((tga (i:load-image #P"test-file.tga")))
+      ;; TGA
+      (ok (eq 'i:image (type-of tga)))
+      (ok (= 128 (i:width tga)))
+      (ok (= 128 (i:height tga)))))
+  (testing "Loading PNG"
+    ;; PNG
+    (ok (signals (i:load-image #P"test-file.png")))))
