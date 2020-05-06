33bfeac7c497a6f983ca7673c30cc173464e727e
alex-eg <alexey.e.egorov@gmail.com>
2020-05-06 13:03:13 +0300

    Add texture coordinate to render-test shader

--- a/render-test/frag.glsl
+++ a/render-test/frag.glsl
@@ -1,10 +1,16 @@
 #version 330
 
-in vec3 color_vert;
+in vec3 vert_color0;
+in vec2 tex_coord0;
+
+uniform sampler2D sampler0;
+
 out vec4 color;
 
-void main ()
+void main()
 {
-    color.xyz = color_vert;
-    color.w = 1.0;
+    color.xyz = vert_color0;
+    if (tex_coord0 != vec2(0.0f)) {
+        color = texture(sampler0, tex_coord0);
+    }
 }
