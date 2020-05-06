c50cef45529fe58888a4de6be1f6e62af13ea685
alex-eg <alexey.e.egorov@gmail.com>
2020-05-06 13:03:13 +0300

    Add texture coordinate to render-test shader

--- a/render-test/vert.glsl
+++ a/render-test/vert.glsl
@@ -2,16 +2,20 @@
 
 layout(location = 0) in vec4 vert_pos;
 layout(location = 1) in vec3 vert_color;
+layout(location = 2) in vec2 tex_coord;
 
 uniform mat4 model_view;
 uniform mat4 projection;
 
-out vec3 color_vert;
+out vec3 vert_color0;
+out vec2 tex_coord0;
 
 void main() {
     gl_Position = (projection * model_view) * vert_pos;
-    color_vert = vert_color;
-    if (color_vert == vec3(0.0f)) {
-        color_vert = vec3(0.1f, 0.4f, 0.4f);
+    vert_color0 = vert_color;
+    if (vert_color0 == vec3(0.0f)) {
+        vert_color0 = vec3(0.1f, 0.4f, 0.4f);
     }
+
+    tex_coord0 = tex_coord;
 }
