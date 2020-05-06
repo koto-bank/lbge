adc1cadce6c63ca5e46fd11834f9fef6569d23b9
alex-eg <alexey.e.egorov@gmail.com>
2020-05-06 13:03:29 +0300

    Add asset package exports

--- src/base/asset/package.lisp
+++ src/base/asset/package.lisp
@@ -3,8 +3,13 @@
   (:local-nicknames (:f :lbge.filesystem)
                     (:u :lbge.utils))
   (:export
+   ;; Key
    :asset-key
    :make-asset-key
+   :asset-type
+   :key-type
+   :path
+   :options
 
    :asset
    :asset-data
@@ -14,10 +19,15 @@
    :asset-manager
    :asset-handler
    :add-handler
+   :handler-get-type
+   :handler-get-asset
+   :find-asset-file-by-path
 
    :add-root
    :asset-roots
    :get-asset
+
+   :find-asset-by-path
 
    :sexp-asset-handler
    :glsl-asset-handler))
