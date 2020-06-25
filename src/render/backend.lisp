2f4e7419e8b0572844d6646edc5ffd361e5848b6
alex-eg <alexey.e.egorov@gmail.com>
2020-06-25 15:17:35 +0300

    Change material interface

--- src/render/backend.lisp
+++ src/render/backend.lisp
@@ -13,12 +13,18 @@ info is custom info, what can be placed there, depends on backend"))
 (defgeneric resize-viewport (backend renderer width height))
 (defgeneric present (backend))
 (defgeneric deinit (backend))
+
+;;; Shaders
 (defgeneric make-shader (backend shader-key))
 (defgeneric shader-list (backend))
 (defgeneric use-shader (backend shader))
+
 ;;; Textures
 (defgeneric make-texture (backend &rest args))
 (defgeneric use-texture (backend shader texture))
+
+;;; Materials
+(defgeneric use-material (backend material))
 
 ;;; Misc
 (defgeneric print-statistics (backend &optional stream args))
