63f68a5efe9a74b445a2b2efede2d0b477633104
alex-eg <alexey.e.egorov@gmail.com>
2020-07-09 17:36:26 +0300

    Bump shader version to 420 in order to use binding locations

--- a/render-test/frag.glsl
+++ a/render-test/frag.glsl
@@ -1,15 +1,14 @@
-#version 330
+#version 420
 
-in vec3 vert_color0;
 in vec2 tex_coord0;
 
-uniform sampler2D sampler0;
+layout(binding = 0) uniform sampler2D sampler0;
 
 out vec4 color;
 
 void main()
 {
-    color.xyz = vert_color0;
+    color.xyz = vec3(1.0, 0.0, 0.0);
     if (tex_coord0 != vec2(0.0f)) {
         color = texture(sampler0, tex_coord0);
     }
--- a/render-test/vert.glsl
+++ a/render-test/vert.glsl
@@ -1,4 +1,4 @@
-#version 330
+#version 420
 
 layout(location = 0) in vec4 vert_pos;
 layout(location = 1) in vec2 tex_coord;
@@ -6,7 +6,6 @@ layout(location = 1) in vec2 tex_coord;
 uniform mat4 model_view;
 uniform mat4 projection;
 
-out vec3 vert_color0;
 out vec2 tex_coord0;
 
 void main() {
