d7eef56f941fb56b3750f98b409ca62bdcd8d592
alex-eg <alexey.e.egorov@gmail.com>
2020-07-12 12:31:01 +0300

    Add shader uniform handling functions to the material

--- src/render/material.lisp
+++ src/render/material.lisp
@@ -8,3 +8,15 @@
    (shader :documentation "Material shader"
            :accessor shader))
   (:documentation "Graphics material"))
+
+(defun add-uniform (material name slot-name)
+  "Name is name uniform name in the shader.
+Slot-name is the name of corresponding slot in the shader"
+  (push (cons name slot-name)
+        (uniforms material)))
+
+(defun set-uniforms (material)
+  "Set all uniform parameters"
+  (with-slots (shader uniforms) material
+    (loop :for (name . slot) :in uniforms :do
+      (s:set-uniform shader name (slot-value material slot)))))
