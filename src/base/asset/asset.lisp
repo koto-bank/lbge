3eba85996b3e1a19c236d64e785dc84b3688dbbb
alex-eg <alexey.e.egorov@gmail.com>
2021-01-20 22:03:51 +0300

    Add metaclass for asset
    The metaclass allows to add `:dep t' argument to slot definition,
    which will mark the slot as current asset dependence.
    
    Later it can be used to traverse all dependencies, which is
    particularly useful for recursive asset loading.

--- src/base/asset/asset.lisp
+++ src/base/asset/asset.lisp
@@ -34,6 +34,79 @@ For :memory may be just an asset name or whatever"
                             :path path
                             :options options))
 
+;;; Metaclass for asset class
+;;; for :dep slot option support, so we can easily get
+;;; all slots containing dependencies for every asset class
+(defclass asset-class (standard-class) ())
+
+(defclass asset-direct-slot (closer-mop:standard-direct-slot-definition)
+  ((dep :initarg :dep
+        :initform nil
+        :accessor direct-slot-asset-dep)))
+
+(defclass asset-effective-slot (closer-mop:standard-effective-slot-definition)
+  ((dep :initarg :dep
+        :initform nil
+        :accessor effective-slot-asset-dep)))
+
+(defmethod closer-mop:direct-slot-definition-class ((class asset-class) &rest initargs)
+  (find-class 'asset-direct-slot))
+
+(defmethod closer-mop:effective-slot-definition-class ((class asset-class) &rest initargs)
+  (find-class 'asset-effective-slot))
+
+(defun metaclass-of (object)
+  (class-of (class-of object)))
+
+(defun is-asset (dependency-slot)
+  (unless dependency-slot
+    (return-from is-asset nil))
+  (eq (class-of
+       (find-class
+        (closer-mop:slot-definition-type dependency-slot)))
+      (find-class 'asset-class)))
+
+(defun get-invalid-deps (dependencies)
+  "Find dependencies which don't have type or it is not an asset class
+defined with `define-asset'"
+  (mapcar (lambda (dep)
+            (let ((type (closer-mop:slot-definition-type dep)))
+              (list (closer-mop:slot-definition-name dep)
+                    type
+                    (if type
+                      (class-of (closer-mop:slot-definition-type dep))
+                      'n/a))))
+          (remove-if #'is-asset dependencies)))
+
+(defmethod closer-mop:compute-effective-slot-definition ((class asset-class) name direct-slots)
+  (let ((slot (call-next-method))
+        (direct-slot (first direct-slots)))
+    (when (eq (class-of direct-slots)
+              (find-class 'asset-direct-slot))
+      (setf (effective-slot-asset-dep slot)
+            (direct-slot-asset-dep direct-slot))) ; override the dep
+    slot))
+
+(defmethod closer-mop:compute-slots ((class asset-class))
+  (let* ((all-slots (call-next-method))
+         (deps (remove-if-not #'identity all-slots :key #'effective-slot-asset-dep)))
+    (assert (every #'is-asset deps)
+            nil "Every asset dependence should have :type which must be defined with `define-asset'
+Dependencies in question:~:{ ~A, type: ~A, class: ~A~}"
+            (get-invalid-deps deps))
+    (cons (make-instance 'asset-effective-slot
+                         :allocation :class
+                         :allocation-class class
+                         :name '%dependencies
+                         :initfunction #'(lambda () deps)
+                         :documentation "Asset dependencies"
+                         :initform deps)
+          all-slots)))
+
+(defmethod closer-mop:validate-superclass ((class asset-class)
+                                           (super standard-class))
+  t)
+
 (defclass asset ()
   ((state
     :documentation
