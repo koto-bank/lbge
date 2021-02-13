2a87dba8914f8ad17381e5b91235afcf6be9135d
alex-eg <alexey.e.egorov@gmail.com>
2021-02-13 14:26:02 +0300

    Divide textured and plain materials in render-test app

--- a/render-test/textured-vert.glsl
+++ a/render-test/textured-vert.glsl
@@ -1,10 +1,14 @@
 #version 330
 
 layout(location = 0) in vec4 vert_pos;
+layout(location = 1) in vec2 tex_coord;
 
 uniform mat4 model_view;
 uniform mat4 projection;
 
+out vec2 tex_coord0;
+
 void main() {
     gl_Position = (projection * model_view) * vert_pos;
+    tex_coord0 = tex_coord;
 }
