6e08ec5457a4e742edce62a53bbc2f5ec6a1c383
alex-eg <alexey.e.egorov@gmail.com>
2021-02-13 14:32:49 +0300

    Implement loading of render assets and init of their backend parts

--- src/base/asset/render-asset-handlers.lisp
+++ src/base/asset/render-asset-handlers.lisp
@@ -2,15 +2,14 @@
 
 ;;; Shader source
 (define-asset shader-source ()
-  ((source :initarg :source :reader shader-source)))
+  ((source :initarg :source)))
 
 (define-asset-handler shader-source (manager key)
   (let ((file-path (find-path-by-path-key manager key)))
     (log:debug "File path ~S" file-path)
     (log:debug "Asset key ~S" key)
     (unless file-path
-      (return (make-asset 'shader-source
-                          key
+      (return (make-asset key
                           :state :error)))
     (let ((shader-lines nil))
       (with-open-file (shader-file
@@ -23,7 +22,7 @@
           (unless (string= "" (u:trim-newlines line))
             (setf shader-lines (cons (format nil "~a~%" line)
                                      shader-lines)))))
-      (make-asset 'shader-source key
+      (make-asset key
                   :source (reverse shader-lines)
                   :state :loaded))))
 
@@ -32,7 +31,7 @@
 
 (define-asset-handler image-asset (manager key)
   (let ((file-path (lbge.asset:find-path-by-path-key manager key))
-        (image-asset  (make-asset 'image-asset key :state :error)))
+        (image-asset  (make-asset key :state :error)))
     (log:debug "File path ~S" file-path)
     (log:debug "Asset key ~S" key)
     (unless file-path
@@ -44,24 +43,72 @@
       image-asset)))
 
 ;;; Texture
-(define-asset texture-asset (lbge.render.texture:texture)
-  ((image-asset :type image-asset :dep t)))
+(define-asset texture-asset ()
+  ((image-asset :type image-asset :dep t)
+   (backend-texture
+    :accessor backend-texture
+    :documentation "Texture object created and managed by rendering
+    backend")))
 
 (define-asset-handler texture-asset (manager key)
   (let ((file-path (find-path-by-path-key manager key))
-        (texture (make-asset 'texture-asset key)))
+        (texture (make-asset key :state :error)))
     (unless file-path
       (return texture))
-    (s:deserialize-file texture file-path)))
+    (s:deserialize-file texture file-path)
+    [texture.state setf :loaded]
+    texture))
 
 ;;; Material
-(define-asset material-asset (lbge.render.material:material)
-  ((vertex-shader-source :dep t :type shader-source-asset)
-   (fragment-shader-source :dep t :type shader-source-asset)))
+(define-asset material-asset ()
+  ((vertex-shader-source :dep t :type shader-source)
+   (fragment-shader-source :dep t :type shader-source)
+   (backend-material
+    :accessor backend-material
+    :documentation "Material created and managed by the rendering backend")))
 
 (define-asset-handler material-asset (manager key)
   (let ((file-path (find-path-by-path-key manager key))
-        (material (make-asset 'material-asset key :state :error)))
+        (material (make-asset key :state :error)))
     (unless file-path
       (return material))
-    (s:deserialize-file material file-path)))
+    (s:deserialize-file material file-path)
+    [material.state setf :loaded]
+    material))
+
+(defun build-shader (backend sources)
+  ;; Sources is a list in format
+  ;; (:vertex <vertex-source>
+  ;;  :fragment <fragment-source>)
+  (let ((shader (b:make-shader backend)))
+    (sh:add-stage shader sources)
+    (sh:compile-shader shader)
+    shader))
+
+(defun make-sampler-keyword (num)
+  (alexandria:make-keyword (format nil "SAMPLER~A" num)))
+
+(defun build-material (backend material textures uniforms)
+  (let ((shader (build-shader
+                 backend
+                 (list :vertex [material.vertex-shader-source.source]
+                       :fragment [material.fragment-shader-source.source])))
+        (backend-material (make-instance 'mat:material)))
+    (setf [material.backend-material] backend-material
+          [backend-material.mat:shader] shader)
+    (loop
+      for (name slot-name) on uniforms by #'cddr
+      do (mat:add-uniform backend-material name
+                          (slot-value material slot-name)))
+    (loop
+      for texture-slot in textures
+      for i from 0
+      do (let ((texture (slot-value material texture-slot)))
+           (setf [texture.backend-texture]
+                 (b:make-texture backend
+                                 :image [texture.image-asset]
+                                 :target :texture-2d
+                                 :format :rgba8))
+           (mat:add-texture backend-material (make-sampler-keyword i)
+                            [texture.backend-texture])))
+    material))
