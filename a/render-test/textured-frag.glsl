2825bb62a96099796d035169e93651a9405b08fd
alex-eg <alexey.e.egorov@gmail.com>
2021-02-13 14:26:02 +0300

    Divide textured and plain materials in render-test app

---
+++ a/render-test/textured-frag.glsl
@@ -0,0 +1,18 @@
+#version 330
+
+in vec2 tex_coord0;
+
+uniform sampler2D sampler0;
+
+uniform vec4 in_color;
+
+out vec4 color;
+
+void main()
+{
+    if (tex_coord0 != vec2(0.0f)) {
+        color = texture(sampler0, tex_coord0);
+    } else {
+        color = in_color;
+    }
+}
