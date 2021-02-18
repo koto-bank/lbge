a5a72eaed01556b4b336f5578707dc1a59042d9f
alex-eg <alexey.e.egorov@gmail.com>
2021-02-18 11:02:47 +0300

    Fix png loading unit test

--- src/image-loader/t/image-loader-test.lisp
+++ src/image-loader/t/image-loader-test.lisp
@@ -7,5 +7,9 @@
       (ok (= 128 (i:width tga)))
       (ok (= 128 (i:height tga)))))
   (testing "Loading PNG"
-    ;; PNG
-    (ok (signals (i:load-image #P"test-file.png")))))
+    (let ((png (i:load-image #P"test-file.png")))
+      ;; PNG
+      (ok (eq 'i:image (type-of png)))
+      (ok (= 272 (i:width png)))
+      (ok (= 170 (i:height png)))
+      (ok (eq :rgb8 (i:channels png))))))
